5dbac5491dddb61a5c3396e6165043e5108d24b8
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2015-10-05 18:15:49 +0000

    testing instagram api configuration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,26 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/redirect/twitter', function(){
+// Get code parameter.
+    $code = Request::get('code');
+// Request the access token.
+    $data = Instagram::getOAuthToken($code);
+
+// Set the access token with $data object.
+    Instagram::setAccessToken($data);
+
+// We're done here - how easy was that, it just works!
+    dd( Instagram::getUserLikes());
+// This example is simple, and there are far more methods available.
+});
+
+Route::get('/test', function(){
+    $loginUrl = Instagram::getLoginUrl();
+    return view('instagram')->with('loginUrl', $loginUrl);
+
+});
+
 Route::group(['prefix' => 'api'], function () { //TODO Add auth middleware later
     Route::get('/', function ()    {
         return response()->json([
